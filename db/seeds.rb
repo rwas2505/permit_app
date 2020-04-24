073e19b705b197f9efbf938068ec7b0da8b7bbba
ryan <ryanwaszak@gmail.com>
2020-04-24 17:51:48 -0500

    add logic to only include offices that are in the states of the sample ahj list

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,24 +9,47 @@ require 'csv'
 
 #generate a defined number of AHJ/State pairs
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'all_ahjs.csv'))
+ahj_csv_text = File.read(Rails.root.join('lib', 'seeds', 'all_ahjs.csv'))
+ahj_csv = CSV.parse(ahj_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+office_csv_text = File.read(Rails.root.join('lib', 'seeds', 'all_offices.csv'))
+office_csv = CSV.parse(office_csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-
-# make each row a hash element in a new array
+# make each row a hash element in a new array for the ahjs
 ahj_list = []
-csv.each do |row|
+ahj_csv.each do |row|
   ahj = row.to_hash
   ahj_list << ahj
 end
 
 # randomly select 10 ahjs from the above array of ~15000 ahjs and display their name and state
-sample_list = ahj_list.sample(10)
-sample_list.each do |ahj|
-  p " AHJ: #{ahj["AHJ"]} STATE: #{ahj["State"]} "
+state_array = []
+ahj_sample_list = ahj_list.sample(10)
+ahj_sample_list.each do |ahj|
+  state_array << ahj["State"]
+  # p " AHJ: #{ahj["AHJ"]} STATE: #{ahj["State"]} "
 end
-p sample_list.count
 
-#generate a defined number of offices based on the states populated from the AHJs above
+#generate all offices that exist in the above states 
+
+office_list = []
+office_csv.each do |row|
+  # p row["Name"]
+  # p row["LocationCode"][0..1]
+  office = row.to_hash
+  office_list << office
+end
+
+#reduce array to only contain offices that exist in the states from the state_array
+
+local_offices = office_list.select{|office| state_array.include?(office["LocationCode"][0..1])}
+# local_offices.each do |office|
+#    p "#{office["Name"]}, #{office["LocationCode"][0..1]}"
+# end
+# verification = local_offices.uniq{|office| office["LocationCode"][0..1]}
+# p verification
+
+
+
+
 
 
