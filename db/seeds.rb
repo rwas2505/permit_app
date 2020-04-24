7b4209bc9e9ce8a832e2270fdb87a2d0de961ce5
ryan <ryanwaszak@gmail.com>
2020-04-24 16:52:13 -0500

    add lib/seeds directory for csv files and generate random sample of AHJ/State pairs in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+#generate a defined number of AHJ/State pairs
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'all_ahjs.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+# make each row a hash element in a new array
+ahj_list = []
+csv.each do |row|
+  ahj = row.to_hash
+  ahj_list << ahj
+end
+
+# randomly select 10 ahjs from the above array of ~15000 ahjs and display their name and state
+sample_list = ahj_list.sample(10)
+sample_list.each do |ahj|
+  p " AHJ: #{ahj["AHJ"]} STATE: #{ahj["State"]} "
+end
+p sample_list.count
+
+#generate a defined number of offices based on the states populated from the AHJs above
+
+
